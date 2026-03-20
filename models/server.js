67c43074498f6184d207f72670c0f759e1031f34
structGust <structgustmail@gmail.com>
2026-03-20 19:08:25 +0100

    20-Desplegar nuestro backend server en Heroku

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
 
     constructor() {
         this.app    = express();
-        this.port   = 8080;
+        this.port   = process.env.PORT;
 
         // Http server
         this.server = http.createServer(this.app);
